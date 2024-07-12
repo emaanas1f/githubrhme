5e09aa00e0d8bfaeb1a53bf24a6782503d707e95
emaan asif <emaanasif438@gmail.com>
2024-07-12 14:59:12 -0400

    Initial Commit

---
+++ githubrhme/githubrhmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubrhmeApp.swift
+//  githubrhme
+//
+//  Created by emaan asif on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct githubrhmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
